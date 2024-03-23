b95d125c903339913320d7645dd2cc5fe835a46e
Alysson-Araujo <alysson10araujo@gmail.com>
2024-03-23 15:03:22 -0300

    addition of spring boot version backend

---
+++ backend-livinfo/src/main/java/com/livinfo/backendlivinfo/BackendLivinfoApplication.java
@@ -0,0 +1,13 @@
+package com.livinfo.backendlivinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLivinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLivinfoApplication.class, args);
+	}
+
+}
---
+++ backend-livinfo/src/test/java/com/livinfo/backendlivinfo/BackendLivinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livinfo.backendlivinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLivinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
